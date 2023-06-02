8bad3a9b8e74c840f1cf42d0a2c27972d65c15b8
VENTURA\ventu <abednego.bogichristian@ai.astra.co.id>
2023-06-02 09:09:54 +0700

    add all program (76%)

---
+++ app/src/main/java/com/personal/privatechat/listeners/ConversionListener.java
@@ -0,0 +1,7 @@
+package com.personal.privatechat.listeners;
+
+import com.personal.privatechat.models.User;
+
+public interface ConversionListener {
+    void onConversionClicked(User user);
+}
---
+++ app/src/main/java/com/personal/privatechat/listeners/UserListener.java
@@ -0,0 +1,7 @@
+package com.personal.privatechat.listeners;
+
+import com.personal.privatechat.models.User;
+
+public interface UserListener {
+    void onUserClicked(User user);
+}
